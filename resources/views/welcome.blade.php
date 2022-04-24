54ad21c5d150748af138e9f2e25de4101e533b2c
rizalblog <rizaldimohammad2002@gmail.com>
2022-04-24 14:38:43 +0700

    tinggal databasenya rek

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>zona futsal bondowoso</title>
+        <title>Gladitor Futsal</title>
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
@@ -19,14 +19,13 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#page-top">ZONA FUTSAL BONDOWOSO</a>
+                <a class="navbar-brand" href="#page-top">Gladiator Futsal</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link" href="pilihan">Pilih Zona Futsal</a></li>
                         <li class="nav-item"><a class="nav-link" href="contact">Contact</a></li>
                         <li class="nav-item"><a class="nav-link" href="login">Login</a></li>
                         <li class="nav-item"><a class="nav-link" href="register">Register</a></li>
@@ -39,9 +38,9 @@
             <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
                 <div class="d-flex justify-content-center">
                     <div class="text-center">
-                        <h1 class="mx-auto my-0 text-uppercase">FUTSAL BONDOWOSO</h1>
+                        <h1 class="mx-auto my-0 text-uppercase">GLADIATOR FUTSAL</h1>
                         <h2 class="text-white-50 mx-auto mt-2 mb-5">Pesan Lapangan Futsal disini dan bermain dengan sportif</h2>
-                        <a class="btn btn-primary" href="#about">Pesan Sekarang</a>
+                        <a class="btn btn-primary" href="#about">Download Aplikasi</a>
                     </div>
                 </div>
             </div>
@@ -51,58 +50,119 @@
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-lg-8">
-                        <h2 class="text-white mb-4">Pilih Lapangan Sesuai Yang Anda Suka</h2>
+                        <h2 class="text-white mb-4">Gladiator Futsal Bondowoso</h2>
                         <p class="text-white-50">
-                            Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
-                            <a href="https://startbootstrap.com/theme/grayscale/">the preview page.</a>
-                            The theme is open source, and you can use it for any purpose, personal or commercial.
+                            Selamat datang di Sistem Informasi penyewaan Lapangan Futsal
                         </p>
                     </div>
                 </div>
-                <img class="img-fluid" src="assets/img/ipad.png" alt="..." />
             </div>
         </section>
+        {{-- Galeri --}}
+        <section class="galeri-section">
+            <div class="container px-4 px-lg-5">
+                <div class="row">
+                    <div class="col-md-10 col-lg-8 mx-auto text-center">
+                        <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
+                        <h2 class="mb-5">Galeri</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-sm-12 mt-2">
+                        <img src="/image/lapangan-futsal-e1500518630769.jpg" class="rounded" alt="...">
+                    </div>
+                    <div class="col-lg-4 col-sm-12 mt-2">
+                        <img src="/image/lapangan-futsal-e1500518630769.jpg" class="rounded" alt="...">
+                    </div>
+                    <div class="col-lg-4 col-sm-12 mt-2">
+                        <img src="/image/lapangan-futsal-e1500518630769.jpg" class="rounded" alt="...">
+                    </div>
+                </div>
+            </div>
+        </section>
+
         <!-- Projects-->
         <section class="projects-section bg-light" id="projects">
             <div class="container px-4 px-lg-5">
-                <!-- Featured Project Row-->
-                <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
-                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="assets/img/bg-masthead.jpg" alt="..." /></div>
-                    <div class="col-xl-4 col-lg-5">
-                        <div class="featured-text text-center text-lg-left">
-                            <h4>Shoreline</h4>
-                            <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
-                        </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h4>Harga Jadwal Lapangan Futsal</h4>
                     </div>
                 </div>
-                <!-- Project One Row-->
-                <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
-                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-01.jpg" alt="..." /></div>
-                    <div class="col-lg-6">
-                        <div class="bg-black text-center h-100 project">
-                            <div class="d-flex h-100">
-                                <div class="project-text w-100 my-auto text-center text-lg-left">
-                                    <h4 class="text-white">Misty</h4>
-                                    <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
-                                    <hr class="d-none d-lg-block mb-0 ms-0" />
-                                </div>
+                <div class="row">
+                    <div class="col-lg-6 col-sm-12 mt-2">
+                        <div class="card">
+                            <div class="card-body">
+                              <h5 class="card-title">Paket Pagi - Sore</h5>
+                              <h6 class="card-subtitle mb-2 text-muted">Jam 08.00 WIB - 17.00 WIB</h6>
+                              <p class="fw-bold">Rp.80.000,- Per Jam</p>
+                              <a href="#" class="btn btn-primary">Pesan Sekarang</a>
                             </div>
-                        </div>
+                          </div>
+                    </div>
+                    <div class="col-lg-6 col-sm-12 mt-2">
+                        <div class="card">
+                            <div class="card-body">
+                              <h5 class="card-title">Paket Malam</h5>
+                              <h6 class="card-subtitle mb-2 text-muted">Jam 19.00 WIB - 00.00 WIB </h6>
+                              <p class="fw-bold">Rp.100.000,- Per Jam</p>
+                              <a href="#" class="btn btn-primary">Pesan Sekarang</a>
+                            </div>
+                          </div>
                     </div>
                 </div>
-                <!-- Project Two Row-->
-                <div class="row gx-0 justify-content-center">
-                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-02.jpg" alt="..." /></div>
-                    <div class="col-lg-6 order-lg-first">
-                        <div class="bg-black text-center h-100 project">
-                            <div class="d-flex h-100">
-                                <div class="project-text w-100 my-auto text-center text-lg-right">
-                                    <h4 class="text-white">Mountains</h4>
-                                    <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
-                                    <hr class="d-none d-lg-block mb-0 me-0" />
-                                </div>
+
+            </div>
+            <div class="container px-4 px-lg-5 mt-4">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h4>Jadwal Booking</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 col-sm-12 mt-2">
+                        <div class="card">
+                            <div class="card-body">
+                              <h5 class="card-title">Jadwal Booking Lapangan</h5>
+                              <table class="table">
+                                <thead>
+                                  <tr>
+                                    <th scope="col">No</th>
+                                    <th scope="col">Nama Penyewa</th>
+                                    <th scope="col">Jam</th>
+                                    <th scope="col">Durasi</th>
+                                    <th scope="col">Tanggal</th>
+                                    <th scope="col">Lapangan</th>
+                                    <th scope="col">Status</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr>
+                                    <th scope="row">1</th>
+                                    <td>Dafrin</td>
+                                    <td>08.00 WIB - 10.00 WIB</td>
+                                    <td>2 Jam</td>
+                                    <td>25/04/2022</td>
+                                    <td>Gladiator 1</td>
+                                    <td>
+                                        <span class="badge rounded-pill bg-success">Booking</span>
+                                    </td>
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">2</th>
+                                    <td>Hasyim</td>
+                                    <td>08.00 WIB - 10.00 WIB</td>
+                                    <td>2 Jam</td>
+                                    <td>25/04/2022</td>
+                                    <td>Gladiator 2</td>
+                                    <td>
+                                        <span class="badge rounded-pill bg-danger">Belum</span>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
                             </div>
-                        </div>
+                          </div>
                     </div>
                 </div>
             </div>
@@ -113,7 +173,7 @@
                 <div class="row gx-4 gx-lg-5">
                     <div class="col-md-10 col-lg-8 mx-auto text-center">
                         <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
-                        <h2 class="text-white mb-5">Zona futsal Bondowoso</h2>
+                        <h2 class="text-white mb-5">Gladiator Futsal Bondowoso</h2>
                         <!-- * * * * * * * * * * * * * * *-->
                         <!-- * * SB Forms Contact Form * *-->
                         <!-- * * * * * * * * * * * * * * *-->
@@ -125,7 +185,7 @@
                             <!-- Email address input-->
                             <div class="row input-group-newsletter">
                                 <div class="col"><input class="form-control" id="emailAddress" type="email" placeholder="Enter email address..." aria-label="Enter email address..." data-sb-validations="required,email" /></div>
-                                <div class="col-auto"><button class="btn btn-primary disabled" id="submitButton" type="submit">Notify Me!</button></div>
+                                <div class="col-auto"><button class="btn btn-primary disabled" id="submitButton" type="submit">Contact Me!</button></div>
                             </div>
                             <div class="invalid-feedback mt-2" data-sb-feedback="emailAddress:required">An email is required.</div>
                             <div class="invalid-feedback mt-2" data-sb-feedback="emailAddress:email">Email is not valid.</div>
@@ -161,7 +221,7 @@
                                 <i class="fas fa-map-marked-alt text-primary mb-2"></i>
                                 <h4 class="text-uppercase m-0">Address</h4>
                                 <hr class="my-4 mx-auto" />
-                                <div class="small text-black-50">jl.raya Situbondo, Bondowoso</div>
+                                <div class="small text-black-50">Bondowoso</div>
                             </div>
                         </div>
                     </div>
@@ -171,7 +231,7 @@
                                 <i class="fas fa-envelope text-primary mb-2"></i>
                                 <h4 class="text-uppercase m-0">Email</h4>
                                 <hr class="my-4 mx-auto" />
-                                <div class="small text-black-50"><a href="#!">zonafutsalbondowoso@gmail.com</a></div>
+                                <div class="small text-black-50"><a href="#!">gladiatorfutsalbondowoso@gmail.com</a></div>
                             </div>
                         </div>
                     </div>
@@ -194,7 +254,7 @@
             </div>
         </section>
         <!-- Footer-->
-        <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; Zona Futsal Bondowoso 2022</div></footer>
+        <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; Gladiator Futsal Bondowoso 2022</div></footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
